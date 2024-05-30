444e2e7399b65050e6659975d7a1996cbe41fe88
Khalil-Elemam <khalil2020100@gmail.com>
2024-05-30 13:00:20 +0300

    update vercel json file

--- anime-list-ui/src/components/Layout.jsx
+++ anime-list-ui/src/components/Layout.jsx
@@ -1,14 +1,13 @@
 
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
-import AnimeList from "./AnimeList"
 
 export default function Layout() {
 
     return (
         <>
             <Header/>
-            <AnimeList />
+            <Outlet />
         </>
     )
 }
